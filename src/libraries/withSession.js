880d09efb2d2888181ade8851e7b4d8013d93b04
Rohanarora17 <rohanarora494@gmail.com>
2023-10-19 18:13:58 +0530

    myways

--- src/libraries/withSession.js
+++ src/libraries/withSession.js
@@ -2,8 +2,8 @@ import { withIronSession } from "iron-session";
 
 export default function withSession(app) {
     return withIronSession(app, {
-        password: "mywaysassignment123",
-        cookieName: "myways-chat",
+        password: "bXlzcWxhc3N3b3JkMTIzNDU2Nzg5MA====",
+        cookieName: "myways",
         cookieOptions: {
             secure: process.env.NODE_ENV === "production",
         }
